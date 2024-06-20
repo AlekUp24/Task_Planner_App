873d1e6c74473f97be499ae564c0bd7ea5e06e8b
AlekUp24 <alekp24@gmail.com>
2024-06-20 14:19:54 +0200

    create/remove tasks and lists completed. Update task to do and some navigation

--- task_planner_app/tasks_app/urls.py
+++ task_planner_app/tasks_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('create_list', views.create_list, name="create_list"),
     path('add_created_list', views.add_created_list, name="add_create_list"),
     path('<int:id>/create_task', views.create_task,  name="create_task"),
+    path('<int:id>/delete_list', views.delete_list,  name="delete_list"),
+    path('<int:id>/add_created_task', views.add_created_task,  name="add_created_task"),
+    path('<int:id>/<int:id_2>/delete_task', views.delete_task,  name="delete_task"),
 ]
--- task_planner_app/tasks_app/views.py
+++ task_planner_app/tasks_app/views.py
@@ -25,14 +25,43 @@ def create_list(request):
 def add_created_list(request):
     if request.method == 'POST':
         new_name = request.POST.get('new_name')
-        print(new_name)
         form = TaskList(name=new_name)    
         form.save()
     
     return redirect("/")
 
+def delete_list(request,id):
+    task_list = TaskList.objects.get(id=id)
+    task_list.delete()
+
+    return redirect("/")
+
 def create_task(request,id):
     task_list = TaskList.objects.get(id=id)
+
     return render(request, 'create_task.html', {"list":task_list})
 
+def add_created_task(request,id):
 
+    if request.method == 'POST':
+        list = TaskList.objects.get(id=id)
+        new_name = request.POST.get('new_name')
+        new_description = request.POST.get('new_description')
+        new_due = request.POST.get('new_due')
+        new_complete = request.POST.get('new_completed')
+        
+        if new_complete=="on":
+            new_complete=True
+        else:
+            new_complete=False
+
+        form = TaskItem(task_list = list, name = new_name, description = new_description, complete = new_complete , due_date = new_due)
+        form.save()
+    
+    return redirect("/"+str(id))
+
+def delete_task(request,id,id_2):
+    task_item = TaskItem.objects.get(id=id_2)
+    task_item.delete()
+
+    return redirect("/"+str(id))
