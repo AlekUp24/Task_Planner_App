62c2d37f23b178e9e3f6676e3fcb806e1dc6d4f9
AlekUp24 <alekp24@gmail.com>
2024-06-21 13:04:59 +0200

    add GO BACK functionality

--- task_planner_app/tasks_app/views.py
+++ task_planner_app/tasks_app/views.py
@@ -7,17 +7,13 @@ def index(request):
 
     return render(request, 'index.html', {"lists":lists})
 
+# lists
+
 def getlist(request,id):
     task_list = TaskList.objects.get(id=id)
     items = TaskItem.objects.filter(task_list__id=id)
 
     return render(request, 'list_details.html', {"list": task_list, "items":items})
-
-def task_details(request,id,id_2):
-    task_list = TaskList.objects.get(id=id)
-    task = TaskItem.objects.get(id=id_2)
-
-    return render(request, 'task_details.html', {"task":task})
 
 def create_list(request):
     return render(request, 'create_list.html', {})
@@ -35,6 +31,14 @@ def delete_list(request,id):
     task_list.delete()
 
     return redirect("/")
+
+# tasks
+
+def task_details(request,id,id_2):
+    task_list = TaskList.objects.get(id=id)
+    task = TaskItem.objects.get(id=id_2)
+
+    return render(request, 'task_details.html', {"task":task})
 
 def create_task(request,id):
     task_list = TaskList.objects.get(id=id)
